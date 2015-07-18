70772f77141e6dc40246f9f0626dceb8bcacef70
Azzam Enajjar <azzam@pdx.edu>
2015-07-18 04:13:58 -0700

    Added splash screen

---
+++ app/src/main/java/edu/pdx/oss/sqliteloginapp/splashscreen.java
@@ -0,0 +1,34 @@
+package edu.pdx.oss.sqliteloginapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class splashscreen extends Activity {
+
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(splashscreen.this, MainActivity.class);
+                startActivity(i);
+                this.finish();
+            }
+            private void finish() {
+            }
+        }, splashInterval);
+    };
+}
